8070bcf4a5363955861443c40b291171b73316e6
Hector <gt12930@GMAIL.com>
2024-07-09 10:07:54 -0500

    feature: etiquetas

--- src/components/etiquetas/BarCode.jsx
+++ src/components/etiquetas/BarCode.jsx
@@ -21,30 +21,32 @@ const BarCode = ({ values }) => {
               <img
                 src={image1}
                 alt="Custom Logo"
-                style={{ marginBottom: "10px", height: "20px" }}
+                style={{ height: "20px" }}
               />
             </div>
             <div style={{ fontSize: "5px" }}>
               <p>AUTORIDAD AUTONOMA DE MAJES</p>
               <p>OFICINA DE CONTROL Y SANEAMIENTO PATRIMONIAL - 2024</p>
+              <hr />
             </div>
             <div>
               <img
                 src={image}
                 alt="Custom Logo"
-                style={{ marginBottom: "10px", height: "20px" }}
+                style={{  height: "20px" }}
               />
             </div>
           </div>
           <Barcode
             value={value.codigo_activo}
-            width={1}
+            width={1.25}
             height={20}
-            fontSize={10}
+            fontSize={12}
             marginTop={1}
             marginBottom={5}
+            
           />
-          <p style={{fontSize:"6px"}}>{value.descripcion}</p>
+          <p style={{fontSize:"8px"}}>{value.descripcion}</p>
         </div>
       ))}
     </>

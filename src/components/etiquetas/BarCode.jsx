d21f760e4feb9e313b9607ad04704a1914100dfe
Hector <gt12930@GMAIL.com>
2024-07-09 10:26:52 -0500

    feature: etiqueta

--- src/components/etiquetas/BarCode.jsx
+++ src/components/etiquetas/BarCode.jsx
@@ -8,7 +8,7 @@ const BarCode = ({ values }) => {
   return (
     <>
       {values?.map((value, index) => (
-      <div key={index} style={{ width: "5cm", height: "2.5cm", textAlign: "center", padding: "2mm", marginBottom: "5mm" }}>
+      <div key={index} style={{ width: "5cm", height: "2.5cm", textAlign: "center", padding: "1mm", marginBottom: "2mm" }}>
       <div
             style={{
               display: "flex",
